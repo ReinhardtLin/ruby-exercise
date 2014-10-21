99d5d0e5566012b6eeabf244cae8e6392bd88bcf
Reinhardt Lin <reinhardt.lin@gmail.com>
2014-10-21 16:51:13 +0800

    Complete OOP Part1 Ex2

---
+++ oop_part1/oop_part1_ex2.rb
@@ -0,0 +1,46 @@
+class MyCar
+	def initialize(year, model, color)
+		@year = year
+		@model = model
+		@color = color
+		@current_speed = 0
+	end
+
+	def speed_up(number)
+		@current_speed += number
+		puts "You push the gas to accelerate to #{number} km/hr."
+	end
+
+	def brake(number)
+		@current_speed -= number
+		puts "You push the brake to decelerate to #{number} km/hr."
+	end
+
+	def current_speed
+		puts "You are now driving at #{@current_speed} km/hr."
+	end
+
+	def stop
+		@current_speed = 0
+		puts "You stop the car."
+	end
+
+	attr_accessor :color
+	attr_reader :year
+end
+
+tercel = MyCar.new(2005, 'toyota tercel', 'silver')
+tercel.speed_up(60)
+tercel.current_speed
+tercel.speed_up(60)
+tercel.current_speed
+tercel.brake(60)
+tercel.current_speed
+tercel.brake(60)
+tercel.current_speed
+tercel.stop
+tercel.current_speed
+
+tercel.color = 'silver'
+puts tercel.color
+puts tercel.year
